95698f0ff8f3e6e2750ba409c0d857ed6fab8ca9
Ronilson Batista <ronilson.batista@orama.com.br>
2021-04-14 23:07:36 -0300

    Saving notes for tasks

--- test_grade/test_grade.py
+++ test_grade/test_grade.py
@@ -7,7 +7,23 @@ import sys
 sys.path.insert(1, f"{project_dir}/../services/")
 from ServiceMapping import SubmittedServiceHandler
 
+class Grade(db.Model):
+    __tablename__ = "grade"
+    id = db.Column(db.Integer, primary_key=True)
+    student_name = db.Column(db.String(256), nullable=False)
+    test_grade = db.Column(db.Integer(), nullable=False)
+    classcode = db.Column(db.String(256), nullable=False)
+    classname = db.Column(db.String(256), nullable=False)
+    task_id = db.Column(db.Integer(), nullable=False)
+    task = db.Column(db.String(256), nullable=False)
+    is_corrected = db.Column(db.Boolean, nullable=False)
 
+class GradeSchema(ma.SQLAlchemyAutoSchema):
+    grade = ma.Nested(Grade, many=True)
+    class Meta:
+        model = Grade
+
+all_grade_task_schema = GradeSchema(many = True)
 
 @app.route("/api/class/grade/code", methods=["GET"])
 def home():
@@ -15,32 +31,34 @@ def home():
 
 @app.route("/api/grade/", methods=["POST"])
 def take_grade():
-    classcode = request.form["classcode"]
-
     _task = SubmittedServiceHandler().get()
-    # task = _task.get('student_name')
 
     return render_template("grade.html",
                            students= _task )
 
-@app.route("/api/submitted/task", methods=["POST"])
+@app.route("/api/submitted/test/grade", methods=["POST"])
 def post_attendence():
-    enum_presence = request.form["presence"]
     new_data = {
         "classcode" : request.form["classcode"],
         "classname" : request.form["classname"],
         "student_name" : request.form["student_name"],
-        "student_id" : int(request.form["student_id"]),
-        "task" : request.form["presence"],
-        "is_corrected" : bool(False)
-
+        "task" : request.form["task"],
+        "task_id" : int(request.form["task_id"]),
+        "test_grade" : int(request.form["grade"]),
+        "is_corrected" : bool(True)
     }
 
-    submitted = Submitted(**new_data)
-    db.session.add(submitted)
+    grade = Grade(**new_data)
+    db.session.add(grade)
     db.session.commit()
     return "OK"
 
+@app.route("/api/grade/list", methods=["GET"])
+def submitted_list():
+    result = Grade.query.all()
+    return jsonify(all_grade_task_schema.dump(result))
 
 if __name__ == "__main__":
+    if not os.path.exists(database_file):
+        db.create_all()
     app.run(host="0.0.0.0", port=12305, debug=True)
